1d8e1cbbd3d2851c0680eb37352f6169a26b69c6
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-18 10:15:00 +0100

    testing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import DeviceList from './components/DeviceList';
+// import Map from './components/Map';
 
 function App() {
   return (
     <div className="flex flex-col items-center justify-center h-screen sm:p-8 md:p-16 lg:p-24">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DeviceList/>
     </div>
   );
 }
