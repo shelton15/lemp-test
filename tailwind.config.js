fa21b81cba95688378f4564532d6d9a79d0b31f8
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-18 04:36:22 +0100

    installing

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
   content: [],
   theme: {
     extend: {},
