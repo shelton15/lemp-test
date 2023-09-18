5aaf36f8862ad94a9656375da04c31c3ea86efda
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-18 10:48:13 +0100

    ...

--- src/services/ApiService.jsx
+++ src/services/ApiService.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import mapboxgl from 'mapbox-gl';
+import DeviceModel from '../models/DeviceModel';
 
 mapboxgl.accessToken = 'YOUR_MAPBOX_ACCESS_TOKEN';
 
@@ -8,6 +9,28 @@ class Map extends React.Component {
     super(props);
     this.mapContainer = React.createRef();
   }
+
+  static getAllDevices = async () => {
+    // Replace with your actual API call to fetch devices
+    const response = await fetch('https://connect.paj-gps.de/api/device');
+    const data = await response.json();
+
+    // Map the response data to DeviceModel instances
+    const devices = data.map((device) => {
+        return new DeviceModel(device.id, device.name);
+    });
+
+    return devices;
+};
+
+static getLastPosition = async (deviceId) => {
+    // Replace with your actual API call to fetch the last position of a device
+    const response = await fetch(
+        `https://connect.paj-gps.de/api/trackerdata/${deviceId}/last_points?lastPoints=`
+
+    );
+        return response;
+};
 
   componentDidMount() {
     this.map = new mapboxgl.Map({
