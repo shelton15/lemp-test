ae08f885300a0f441436cc803bed5fcd31b5e3b0
shelton15 <88257757+shelton15@users.noreply.github.com>
2023-09-22 23:05:35 +0100

    navigate working

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginForm from './components/LoginForm';
 import DeviceList from './components/DeviceList';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const navigate = useNavigate();
 
   const handleLogin = () => {
     // Perform login logic
     // Set the isLoggedIn state to true upon successful login
     setIsLoggedIn(true);
-    navigate('/devices'); // Navigate to the '/devices' route after successful login
   };
 
   return (
@@ -19,15 +17,17 @@ function App() {
       <div>
         <h1>App</h1>
 
-        <Route exact path="/">
-          {isLoggedIn ? (
-            <Route path="/devices">
-              <DeviceList />
-            </Route>
-          ) : (
-            <LoginForm onLogin={handleLogin} />
-          )}
-        </Route>
+        <Routes>
+          <Route
+            path="/"
+            element={isLoggedIn ? <Navigate to="/devices" /> : <LoginForm onLogin={handleLogin} />}
+          />
+
+          <Route
+            path="/devices"
+            element={isLoggedIn ? <DeviceList /> : <Navigate to="/" />}
+          />
+        </Routes>
       </div>
     </Router>
   );
--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/tailwind.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    {/* <BrowserRouter> */}
       <App/>
-    </BrowserRouter>
+    {/* </BrowserRouter> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
